6315f6e0851f001d4852d1fa084b450991727eeb
Davi Kirchmaier Paiva <davi.kirchmaier99@gmail.com>
2023-01-24 21:40:10 -0300

    faz uns bagulho ai ze

--- src/br/ufjf/dcc/poo/exercicios/Exercicio08.java
+++ src/br/ufjf/dcc/poo/exercicios/Exercicio08.java
@@ -7,11 +7,18 @@ public class Exercicio08 {
         int num;
         num = Integer.parseInt(JOptionPane.showInputDialog("Digite o numero: "));
 
-        int numSort = (int) Math.random() * 100;
+        int numSort = (int) (Math.random() * 100);
+        while (num != numSort){
 
-        if (num == numSort)
-            JOptionPane.showMessageDialog(null, "voce acertou, seu cagao do caralho! ");
-        else
-            JOptionPane.showMessageDialog(null, "passa outro dia, otario");
+            if (num == numSort)
+                JOptionPane.showMessageDialog(null, "voce acertou, seu cagao do caralho! ");
+            else if(num > numSort)
+                JOptionPane.showMessageDialog(null, "passa outro dia, otario, pecou pelo excesso");
+            else
+                JOptionPane.showMessageDialog(null, "passa outro dia, otario, pecou pela falta");
+            num = Integer.parseInt(JOptionPane.showInputDialog("Digite o numero: "));
+            numSort = (int) (Math.random() * 100);
+        }
+
     }
 }
